3486affbd4bcf0b911bc981e2d5fbcc4118d50e5
morn0401 <148730782+morn0401@users.noreply.github.com>
2024-01-17 09:45:27 +0900

    Create delete-output.php

---
+++ delete-output.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Player Deletion</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+
+    <div class="container">
+
+        <?php
+        $pdo = new PDO('mysql:host=mysql219.phy.lolipop.lan;dbname=LAA1516798-final;charset=utf8', 'LAA1516798', 'Pass0401');
+        $sql = $pdo->prepare('delete from player where id=?');
+        $sql->execute([$_POST['id']]);
+        echo '<p class="success-message">選手を削除しました</p>';
+        ?>
+
+        <a href="index.php" class="back-link">一覧へ戻る</a>
+
+    </div>
+
+</body>
+
+</html>
