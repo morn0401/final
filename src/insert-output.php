86a542af36a062dcb26809d0ce4b57a8e7110171
morn0401 <148730782+morn0401@users.noreply.github.com>
2024-01-17 10:16:51 +0900

    Update insert-output.php

--- src/insert-output.php
+++ src/insert-output.php
@@ -75,7 +75,7 @@
         $result2 = $sql2->fetch(PDO::FETCH_ASSOC);
         echo '<p>ポジション: ' . $result2['position_name'] . '</p>';
         ?>
-        <p><a href="index1.php">一覧に戻る</a></p>
+        <p><a href="index.php">一覧に戻る</a></p>
     </div>
 
 </body>
